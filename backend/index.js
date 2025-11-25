af61cfa9b0b95f259d5ed9c44ab61d27d1a91fab
Aman Parida <amantechz467@gmail.com>
2025-11-25 18:14:52 +0530

    socket.io initiated

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import cors from "cors";
+import { Server } from 'socket.io';
+import { createServer } from "http";
 
 const app = express();
+const server = createServer(app);
 const port = 3000;
 
 app.use(cors({
@@ -16,6 +19,21 @@ app.get("/", (req, res) => {
     });
 });
 
-app.listen(port, () => {
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log(`User connected: `, socket.id);
+
+    socket.emit("welcome", `Welcome to server ${socket.id}`);
+
+});
+
+server.listen(port, () => {
     console.log(`Server running at port ${port}`);
 });
